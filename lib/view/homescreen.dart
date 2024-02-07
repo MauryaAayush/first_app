7f6d75441517ca54b1816fff476a7d72a7a9c2f8
MauryaAayush <maayush235@gmail.com>
2024-02-07 12:28:10 +0530

    Radhey Radhey

--- lib/view/homescreen.dart
+++ lib/view/homescreen.dart
@@ -141,70 +141,68 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
             Container(
-              child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 15),
-                height: height * 0.21,
-                width: width,
-                decoration: BoxDecoration(
-                    color: const Color(0xFF35383F),
-                    borderRadius: BorderRadius.circular(30)),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 25, vertical: 20),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '25%',
+              margin: const EdgeInsets.symmetric(horizontal: 15),
+              height: height * 0.21,
+              width: width,
+              decoration: BoxDecoration(
+                  color: const Color(0xFF35383F),
+                  borderRadius: BorderRadius.circular(30)),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 25, vertical: 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '25%',
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                            Text(
+                              "Today's Special!",
+                              style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                            ),
+                            const SizedBox.square(
+                              dimension: 10,
+                            ),
+                            Text(
+                                "Get discount for every\norder.only valid for today",
                                 style: GoogleFonts.poppins(
                                   textStyle: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 40,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              Text(
-                                "Today's Special!",
-                                style: GoogleFonts.poppins(
-                                  textStyle: const TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ),
-                              const SizedBox.square(
-                                dimension: 10,
-                              ),
-                              Text(
-                                  "Get discount for every\norder.only valid for today",
-                                  style: GoogleFonts.poppins(
-                                    textStyle: const TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 11,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ))
-                            ],
-                          ),
+                                    color: Colors.white70,
+                                    fontSize: 11,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ))
+                          ],
                         ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 10),
-                          height: height * 0.2,
-                          width: width * 0.3,
-                          // color: Colors.blue,
-                          child: Image.asset('assets/images/sofa.png'),
-                        ),
-                      ],
-                    ),
-                    Row()
-                  ],
-                ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 10),
+                        height: height * 0.2,
+                        width: width * 0.3,
+                        // color: Colors.blue,
+                        child: Image.asset('assets/images/sofa.png'),
+                      ),
+                    ],
+                  ),
+                  Row()
+                ],
               ),
             ),
             Container(
